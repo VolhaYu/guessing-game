a1e0916967326430bdaa1e13e12c169b14002c40
VolhaYu <olenka_glushko1993@mail.ru>
2022-08-15 00:13:18 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.max + this.min) / 2);
+        return this.middle; 
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
+        this.min = this.middle;
 
     }
+   
 }
+// GuessingGame();
 
 module.exports = GuessingGame;
